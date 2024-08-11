441fa2bf6698418e339a9904ee6146c7c5eb4bc7
dough10 <doughten.jimi@gmail.com>
2024-08-11 18:41:45 -0500

    do not return in this loop

--- util/testStreams.js
+++ util/testStreams.js
@@ -81,7 +81,7 @@ async function testStreams(db) {
   const stations = await db.find({}).toArray();
   let total = 0;
   for (const station of stations) {
-    if (!station) return;
+    if (!station) continue;
     const stream = await isLiveStream(rmRef(station.url));
     if (!stream) continue;
     if (stream.bitrate && stream.bitrate.length > 3) stream.bitrate = stream.bitrate.split(',')[0];
