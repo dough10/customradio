ddb34a9f6d6b0cd89e7bdf5e52847ea8639b0837
dough10 <doughten.jimi@gmail.com>
2024-08-27 09:39:58 -0500

    update build

---
+++ build-container.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+VERSION=$(jq -r '.version' package.json)
+
+if [ $? -ne 0 ] || [ -z "$VERSION" ]; then
+  echo "Error: Unable to read version from package.json"
+  exit 1
+fi
+
+
+docker build -t "$1:$VERSION" -t "$1:latest" .
+docker push "$1:$VERSION" && docker push "$1:latest"
