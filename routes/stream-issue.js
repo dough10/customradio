7e82a15d8b67d93d35349c839675d7e10aa948e8
dough10 <doughten.jimi@gmail.com>
2024-08-31 07:40:50 -0500

    comments

--- routes/stream-issue.js
+++ routes/stream-issue.js
@@ -2,6 +2,24 @@ const {validationResult} = require('express-validator');
 
 const log = require('../util/log.js');
 
+/**
+ * An endpoint for audio stream playback error callback
+ * 
+ * When a stream fails to play frontend will capture URL that caused the issue and post it here for manual check.
+ * 
+ * @function
+ * @param {express.Request} req - The request object.
+ * @param {express.Response} res - The response object.
+ * 
+ * @param {express.Request} req.body - The body of the request.
+ * @param {string} req.body.url - The URL of the station. Must be a valid URL.
+ * @param {string} req.body.error - The error.message string from frontend.
+ * 
+ * @returns {Promise<void>} - A promise that resolves when the response has been sent.
+ * 
+ * @throws {express.Response} 400 - If validation fails or required fields are missing.
+ * @throws {express.Response} 500 - If an error occurs while adding the station with the issue to the database.
+ */
 module.exports = async (db, req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
