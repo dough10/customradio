0318ccc5700fc15c02e426a1eb448aede6eddc31
dough10 <doughten.jimi@gmail.com>
2024-08-31 12:27:32 -0500

    i broke it

--- src/tests/createStationElement.test.js
+++ src/tests/createStationElement.test.js
@@ -13,7 +13,7 @@ describe('createStationElement', () => {
     // Check the li element
     expect(li).to.exist;
     expect(li.tagName).to.equal('LI');
-    expect(li.dataset.title).to.equal(name);
+    expect(li.dataset.name).to.equal(name);
     expect(li.dataset.url).to.equal(url);
     expect(li.dataset.bitrate).to.equal(bitrate.toString());
 
