1f2c157f1d9efc189fccb2341aa3625e3608ee71
dough10 <doughten.jimi@gmail.com>
2024-09-02 06:24:04 -0500

    try catch will keep app from crashing

--- util/saveToCollection.js
+++ util/saveToCollection.js
@@ -7,7 +7,12 @@ module.exports = async (stats) => {
   const url = process.env.DB_HOST || 'mongodb://127.0.0.1:27017';
   const connector = new DbConnector(url, 'statistics');
   const db = await connector.connect();
-  await db.insertOne(stats);
-  log('database statistics saved');
+  console.log(stats);
+  try{
+    await db.insertOne(stats);
+    log('database statistics saved');
+  } catch(error) {
+    console.error('Error saving statictics', error.message);
+  }
   await connector.disconnect();
 };
