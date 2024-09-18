a24e309927b5e1f1b267ec34a04dbf5300c901c4
dough10 <doughten.jimi@gmail.com>
2024-09-18 08:00:49 -0500

    csp-report to json

--- index.js
+++ index.js
@@ -91,6 +91,41 @@ app.use((req, res, next) => {
 });
 
 /**
+ * Middleware to handle Content Security Policy (CSP) violation reports.
+ * 
+ * This middleware checks if the incoming request is of the type `application/csp-report`.
+ * If so, it listens for data chunks, aggregates them into a complete body,
+ * and then attempts to parse the body as JSON. If parsing succeeds, the JSON object
+ * is assigned to `req.body`. If parsing fails, a 400 error response is sent back to the client.
+ * For all other request types, the middleware simply calls `next()` to pass control to the next middleware.
+ * 
+ * @param {import('express').Request} req - The HTTP request object.
+ * @param {import('express').Response} res - The HTTP response object.
+ * @param {function} next - The next middleware function in the stack.
+ * 
+ * @returns {void} This function does not return a value, but may send a response or call next().
+ */
+app.use((req, res, next) => {
+  if (req.is('application/csp-report')) {
+    let body = '';
+    req.on('data', chunk => {
+      body += chunk.toString();
+    });
+    req.on('end', () => {
+      try {
+        req.body = JSON.parse(body);
+        next();
+      } catch (error) {
+        return res.status(400).json({ error: 'Invalid JSON' });
+      }
+    });
+  } else {
+    next();
+  }
+});
+
+
+/**
  * Middleware function to validate API access tokens.
  * 
  * This middleware checks the `Authorization` header of the incoming request for a Bearer token.
