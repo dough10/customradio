3462a5d05951d244575c5239329b59eb7e5c5750
dough10 <doughten.jimi@gmail.com>
2024-10-01 21:40:49 -0500

    more adaptable

--- util/saveToCollection.js
+++ util/saveToCollection.js
@@ -1,15 +1,13 @@
 require('dotenv').config();
 
-const log = require('./log.js');
 const DbConnector = require('./dbConnector.js');
 
-module.exports = async (stats) => {
+module.exports = async (obj, collection) => {
   const url = process.env.DB_HOST || 'mongodb://127.0.0.1:27017';
-  const connector = new DbConnector(url, 'statistics');
+  const connector = new DbConnector(url, collection);
   const db = await connector.connect();
   try{
-    await db.insertOne(stats);
-    log('database statistics saved');
+    await db.insertOne(obj);
   } catch(error) {
     console.error('Error saving statictics', error.message);
   }
