2813dd298ef71d764bdc9c83a7b99240c7c6389e
dough10 <doughten.jimi@gmail.com>
2024-10-06 11:50:37 -0500

    add genres to database

--- routes/stations.js
+++ routes/stations.js
@@ -50,8 +50,8 @@ module.exports = async (db, redis, req, res) => {
 
   const genreString = genres.join(',');
 
-  if (genreString) {
-    // await saveToCollection(, 'genres');
+  if (genres.length) {
+    await saveToCollection(genres, 'genres');
   }
 
   const cacheKey = `stations_${genreString}`;
