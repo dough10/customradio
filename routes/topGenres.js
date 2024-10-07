54385fac31d620b5299035514c0000640957cdea
dough10 <doughten.jimi@gmail.com>
2024-10-07 16:48:22 -0500

    caching doesn't improve experiance

--- routes/topGenres.js
+++ routes/topGenres.js
@@ -28,24 +28,6 @@ const DbConnector = require('../util/dbConnector.js');
  * app.get('/topGenres', getTopGenres);
  */
 module.exports = async (redis, req, res) => {
-  const cacheKey = `topGenres`;
-
-  try {
-    const cachedStations = await redis.get(cacheKey);
-    
-    if (cachedStations) {
-      const stations = JSON.parse(cachedStations);
-      res.set('content-type', 'application/json');
-      log(`${req.ip} -> /topGenres cached`);
-      return res.send(stations);
-    }
-  } catch(error) {
-    console.error('Error getting genres', error.message);
-    res.status(500).json({
-      error: `Error getting genres ${error.message}`
-    });
-  }
-
   const url = process.env.DB_HOST || 'mongodb://127.0.0.1:27017';
   const connector = new DbConnector(url, 'genres');
   const db = await connector.connect();
@@ -64,11 +46,8 @@ module.exports = async (redis, req, res) => {
         $limit: 10
       }
     ]).toArray();
-    log(`${req.ip} -> /topGenres`);
-    console.log(topGenres);
     const genreObj = topGenres.map(obj => obj._id).sort((a, b) => a.localeCompare(b));
     res.json(genreObj);
-    await redis.set(cacheKey, JSON.stringify(genreObj), 'EX', 3600);
   } catch(error) {
     console.error('Error getting genres', error.message);
     res.status(500).json({
