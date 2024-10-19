294ab88a8937b764e202ffdaf4a35edb4e589f50
dough10 <doughten.jimi@gmail.com>
2024-10-19 03:50:49 -0500

    disallow api endpoints

--- routes/robots.js
+++ routes/robots.js
@@ -3,7 +3,15 @@ const log = require('../util/log.js');
 module.exports = (req, res) => {
   const strings = [
     `User-agent: *`,
-    `Allow: /`,
+    'Disallow: /metrics',
+    'Disallow: /stream-issue',
+    'Disallow: /mark-duplicate',
+    'Disallow: /topGenres',
+    'Disallow: /stations',
+    'Disallow: /add',
+    'Disallow: /csp-report',
+    '',
+    `Allow: /$`,
     `Sitemap: ${req.protocol}://${req.get('host')}/sitemap.xml`
   ];
   const joinedStrings = strings.join('\n');
