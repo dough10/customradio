e804ebec8ac8b617671a0d5c5c3f8a074aaf08f5
dough10 <doughten.jimi@gmail.com>
2024-10-20 09:53:55 -0500

    update urls in service workder in postbuild process

---
+++ build_assets/update-sw-urls.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+
+const htmlFolder = path.join(__dirname, '..', 'html');
+const workerFilePath = path.join(htmlFolder, 'worker.js');
+
+let workerFileContent = fs.readFileSync(workerFilePath, 'utf8');
+let files = fs.readdirSync(htmlFolder);
+let screenshots = fs.readdirSync(path.join(htmlFolder, 'screenshots'));
+
+/**
+ * remove things that should not be in the list
+ */
+[
+  'worker.js',
+  'bundle.min.js.LICENSE.txt',
+  'screenshots'
+].forEach(file => files.splice(files.indexOf(file),1));
+
+/**
+ * correct file path strings
+ */
+files = files.map(file => `/${file}`);
+screenshots = screenshots.map(image => `/screenshots/${image}`);
+
+const urlsToCache = JSON.stringify([...files, ...screenshots]);
+
+const newWorkerFileContent = workerFileContent.replace(
+  /(,urlsToCache=)\[[\s\S]*?\];/, 
+  `,urlsToCache=${urlsToCache};`
+);
+fs.writeFileSync(workerFilePath, newWorkerFileContent, 'utf8');
