9dcfff8e40b1011df4bfa5967461a5adc86597af
dough10 <doughten.jimi@gmail.com>
2025-01-12 06:09:03 -0600

    no need to keep that long, disconnect wasn't in the right place

--- util/cleanUpGenres.js
+++ util/cleanUpGenres.js
@@ -3,14 +3,14 @@ const log = require('./log.js');
 
 module.exports = async () => {
   try{
-    const timestamp = new Date().getTime() - 2592000000;
+    const timestamp = new Date().getTime() - 1296000000;
     const url = process.env.DB_HOST || 'mongodb://127.0.0.1:27017';
     const connector = new DbConnector(url, 'genres');
     const db = await connector.connect();
     const result = await db.deleteMany({ time: { $lt: timestamp } });
     log(`${result.deletedCount} genre entry(s) were deleted.`);
+    await connector.disconnect();
   } catch(error) {
     console.error('Error cleaning up genres', error.message);
   }
-  await connector.disconnect();
 };
