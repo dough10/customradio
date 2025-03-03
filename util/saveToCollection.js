b6b493d1f655d1db440c77999cdb0b089f291f5b
dough10 <doughten.jimi@gmail.com>
2025-03-03 06:27:58 -0600

    fix saveToCollection to ensure database connection is closed in finally block

--- util/saveToCollection.js
+++ util/saveToCollection.js
@@ -5,11 +5,12 @@ const DbConnector = require('./dbConnector.js');
 module.exports = async (obj, collection) => {
   const url = process.env.DB_HOST || 'mongodb://127.0.0.1:27017';
   const connector = new DbConnector(url, collection);
-  const db = await connector.connect();
   try{
+    const db = await connector.connect();
     await db.insertOne(obj);
   } catch(error) {
     console.critical('Error saving statictics', error.message);
+  } finally {
+    await connector.disconnect();
   }
-  await connector.disconnect();
 };
