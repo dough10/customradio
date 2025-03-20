aa965395cb1dab124f874e0c07b8f7b373b0586a
dough10 <doughten.jimi@gmail.com>
2025-03-20 09:35:37 -0500

    refactor: add documentation for robots.txt endpoint parameters

--- routes/endpoints/robots.js
+++ routes/endpoints/robots.js
@@ -3,6 +3,12 @@ const Logger = require('../../util/logger.js');
 const logLevel = process.env.LOG_LEVEL || 'info';
 const log = new Logger(logLevel);
 
+/**
+ * robots.txt endpoint
+ * 
+ * @param {Object} req 
+ * @param {Object} res 
+ */
 module.exports = (req, res) => {
   const strings = [
     `User-agent: *`,
