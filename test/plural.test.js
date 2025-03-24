ec733d1f2f6cc76ba0de07bc23e6da3cb0a5ac7e
dough10 <doughten.jimi@gmail.com>
2025-03-24 11:25:33 -0500

    test: add unit test for plural function to validate singular case

--- test/plural.test.js
+++ test/plural.test.js
@@ -5,6 +5,9 @@
   describe('plural string', () => {
     it('should return "s" for any number other than 1', () => {
       [0, 2, 3, 4, 5, 6].forEach(val => expect(plural(val)).to.equal('s'));
+    });
+
+    it('should return an empty string for the number 1', () => {
       expect(plural(1)).to.equal('');
     });
   });
