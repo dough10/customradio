d8cee8b9eecc3aa16f18886502c95dbd31caf1ab
dough10 <doughten.jimi@gmail.com>
2025-04-02 13:08:25 -0500

    refactor: export stamp function for improved module usability

--- src/js/utils/timestamp.js
+++ src/js/utils/timestamp.js
@@ -5,10 +5,8 @@
  * 
  * @returns {String}
  */
-function stamp() {
+export default function stamp() {
   const now = new Date();
   const formattedDate = now.toISOString().split('T')[0];
   return `# created by ${window.location.origin} [${formattedDate}]\n`;
 }
-
-export { stamp };
