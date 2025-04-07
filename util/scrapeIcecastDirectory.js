3b840966484caa1cacb580791a85c64e750ec1a0
dough10 <doughten.jimi@gmail.com>
2025-04-07 17:20:49 -0500

    refactor: optimize Icecast directory scraping with concurrency and retry logic for homepage connection

--- util/scrapeIcecastDirectory.js
+++ util/scrapeIcecastDirectory.js
@@ -2,14 +2,17 @@ require('dotenv').config();
 const xml2js = require('xml2js');
 const axios = require('axios');
 const pack = require('../package.json');
+const pLimit = require('p-limit');
 
 const { testHomepageConnection, plural, msToHhMmSs } = require('./testStreams.js');
 const rmRef = require('./rmRef.js');
 const isLiveStream = require('./isLiveStream.js');
 const usedTypes = require("./usedTypes.js");
+const retry = require('./retry.js');
 const Logger = require('./logger.js');
 const Stations = require('../model/Stations.js');
 
+const limit = pLimit(5);
 const logLevel = process.env.LOG_LEVEL || 'info';
 const log = new Logger(logLevel);
 
@@ -51,42 +54,46 @@ module.exports = async () => {
     const length = data.length;
     
     log.info(`Scraping Icecast Directory for new stations. ${length} stations pulled`);
-    
-    const testedHomepages = [];
-    for (const entry of data) {
-      log.debug(`Scrape progress: ${((data.indexOf(entry) / length) * 100).toFixed(3)}%`);
-      
-      const url = rmRef(entry.listen_url[0]);
-      const stream = await isLiveStream(url);
-    
-      if (!stream.ok) continue;
-      if (!usedTypes.includes(stream.content)) continue;
-      if (await sql.exists(stream.url)) continue;
-    
-      const result = await sql.addStation({
-        name: stream.name || entry.server_name[0] || stream.description,
-        url: stream.url,
-        genre: stream.icyGenre || entry.genre[0] || 'Unknown',
-        online: stream.isLive,
-        'content-type': stream.content || '',
-        bitrate: stream.bitrate || 0,
-        icon: 'Unknown',
-        homepage: await testHomepageConnection(stream.icyurl, testedHomepages) || 'Unknown',
-        error: '',
-        duplicate: false
-      });
-      if (result !== 'Station exists') {
-        log.debug(`Added station: ${result}`);
-        total += 1;
-      }
-    }
+    let counter = 0;
+    await Promise.all(
+      data.map(entry =>
+        limit(async () => {
+          counter++;
+          log.debug(`Scrape progress: ${((counter / length) * 100).toFixed(3)}%`);
+          
+          try{
+            const url = rmRef(entry.listen_url[0]);
+            const stream = await isLiveStream(url);
+          
+            if (!stream.ok) return;
+            if (!usedTypes.includes(stream.content)) return;
+          
+            const result = await sql.addStation({
+              name: stream.name || entry.server_name[0] || stream.description,
+              url: stream.url,
+              genre: stream.icyGenre || entry.genre[0] || 'Unknown',
+              online: stream.isLive,
+              'content-type': stream.content || '',
+              bitrate: stream.bitrate || 0,
+              icon: 'Unknown',
+              homepage: await retry(() => testHomepageConnection(stream.icyurl)) || 'Unknown',
+              error: '',
+              duplicate: false
+            });
+            if (result !== 'Station exists') {
+              log.debug(`Added station: ${result}`);
+              total += 1;
+            }
+          } catch(e) {
+            log.error(`Error processing entry: ${err.message}`);
+          }
+        })
+      )
+    );
     const stats = await sql.dbStats();
     const now = new Date().getTime();
     const ms = now - startTime;
-    stats.timeCompleted = now;
-    stats.duration = ms;
     log.info(`Icecast Directory scrape complete: ${total} entry${plural(total)} added over ${msToHhMmSs(ms)}. usable entries: ${stats.total}, online: ${stats.online}, offline: ${stats.total - stats.online}`);
-    // await saveStats(stats);
   } catch (err) {
     log.critical(`Scrape failed: ${err.message}`);
   } finally {
