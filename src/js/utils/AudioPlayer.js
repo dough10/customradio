ba1a0386aea614c565abdef362df8824ce146b88
dough10 <doughten.jimi@gmail.com>
2025-04-08 17:41:50 -0500

    feat: add AudioPlayer class with volume control and playback features

--- src/js/utils/AudioPlayer.js
+++ src/js/utils/AudioPlayer.js
@@ -18,6 +18,16 @@ const saveVolume = debounce(value => {
  * AudioPlayer class
  */
 export default class AudioPlayer {
+  _selectors = {
+    player: '.player',
+    name: '#name',
+    bitrate: '#bitrate',
+    volume: '#vol',
+    smallButton: '.player>.small-button',
+    stations: '#stations>li',
+    icon: '.player>.small-button>svg>path'
+  };
+
   constructor() {
     this.player = new Audio();
     this.pauseTimer = 0;
@@ -27,6 +37,13 @@ export default class AudioPlayer {
     this.player.onplay = this._onplay.bind(this);
     this.player.onpause = this._onpause.bind(this);
     this.player.ontimeupdate = this._ontimeupdate.bind(this);
+    this._togglePlay = this._togglePlay.bind(this);
+    this._scrollToStation = this._scrollToStation.bind(this);
+    this._setVolume = this._setVolume.bind(this);
+    this._onKeyPress = this._onKeyPress.bind(this);
+
+    this._bouncedToggle = debounce(this._togglePlay, 100);
+
     this.player.onerror = _ => {
       const error = this.player.error;
       const message = error ? error.message : 'Unknown error';
@@ -42,7 +59,7 @@ export default class AudioPlayer {
    * @returns {void}
    */
   _onwaiting() {
-    const icon = document.querySelector('.player>.small-button>svg>path');
+    const icon = document.querySelector(this._selectors.icon);
     if (!icon) return;
     icon.setAttribute('d', 'M480-160q-134 0-227-93t-93-227q0-134 93-227t227-93q69 0 132 28.5T720-690v-110h80v280H520v-80h168q-32-56-87.5-88T480-720q-100 0-170 70t-70 170q0 100 70 170t170 70q77 0 139-44t87-116h84q-28 106-114 173t-196 67Z');
     icon.parentElement.classList.add('spin');
@@ -56,7 +73,7 @@ export default class AudioPlayer {
    * @returns {void}
    */
   _onplaying() {
-    const icon = document.querySelector('.player>.small-button>svg>path');
+    const icon = document.querySelector(this._selectors.icon);
     if (!icon) return;
     icon.parentElement.classList.remove('spin');
     icon.setAttribute('d', 'M520-200v-560h240v560H520Zm-320 0v-560h240v560H200Zm400-80h80v-400h-80v400Zm-320 0h80v-400h-80v400Zm0-400v400-400Zm320 0v400-400Z');
@@ -85,7 +102,7 @@ export default class AudioPlayer {
    */
   _onpause() {
     this.pauseTimer = setTimeout(this._clearPlaying, PAUSE_TIMER_DURATION);
-    const icon = document.querySelector('.player>.small-button>svg>path');
+    const icon = document.querySelector(this._selectors.icon);
     if (!icon) return;
     icon.parentElement.classList.remove('spin');
     icon.setAttribute('d', 'M320-200v-560l440 280-440 280Zm80-280Zm0 134 210-134-210-134v268Z');
@@ -99,7 +116,7 @@ export default class AudioPlayer {
    * @returns {void} 
    */
   _ontimeupdate() {
-    this.currentPlayingElement = document.querySelector(`li[data-url="${this.player.src}"]`);
+    this.currentPlayingElement = document.querySelector(`#stations>li[data-url="${this.player.src}"]`);
 
     if (this.currentPlayingElement && this.currentPlayingElement.hasAttribute('playing')) return;
     
@@ -109,10 +126,13 @@ export default class AudioPlayer {
     if (!this.currentPlayingElement) return;
     
     this.currentPlayingElement.toggleAttribute('playing');
-    document.querySelector('#name').addEventListener('click', _ => this.currentPlayingElement.scrollIntoView({
+  }
+
+  _scrollToStation() {
+    this.currentPlayingElement.scrollIntoView({
       behavior: 'smooth',
       block: 'start'
-    }));
+    })
   }
 
   /**
@@ -143,11 +163,18 @@ export default class AudioPlayer {
     const slider = document.querySelector('#vol>input');
     slider.value = Number(localStorage.getItem('volume')) || 100;
     this.player.volume = slider.value / 100;
+    slider.addEventListener('input', this._setVolume);
+  }
 
-    slider.addEventListener('input', _ => {
-      this.player.volume = slider.value / 100;
-      saveVolume(slider.value);
-    });
+  /**
+   * set the volume on change
+   * 
+   * @param {Event} ev 
+   */
+  _setVolume(ev) {
+    const slider = ev.target;
+    this.player.volume = slider.value / 100;
+    saveVolume(slider.value);
   }
 
   /**
@@ -179,7 +206,7 @@ export default class AudioPlayer {
       const res = await fetch(`${parsed.origin}/currentsong`);
       if (res.status !== 200) return;
       const playing = await res.text();
-      if (playing) document.querySelector('#name').textContent = playing;
+      if (playing) document.querySelector(this._selectors.name).textContent = playing;
     } catch(e) {
       console.error(`error getting now playing: ${e}`);
     }
@@ -197,9 +224,16 @@ export default class AudioPlayer {
    * @returns {void} 
    */
   playStream({id, url, name, bitrate}) {
-    document.querySelector('#name').textContent = name;
-    document.querySelector('#bitrate').textContent = `${bitrate === 0 ? '???' : bitrate}kbps`;
-    const miniPlayer = document.querySelector('.player');
+    if (!id || !url || !name) {
+      console.error('Invalid station data:', { id, url, name, bitrate });
+      new Toast('Invalid station data. Unable to play stream.', 3);
+      return;
+    }
+
+    document.querySelector(this._selectors.name).textContent = name;
+    document.querySelector(this._selectors.bitrate).textContent = `${bitrate === 0 ? '???' : bitrate}kbps`;
+    
+    const miniPlayer = document.querySelector(this._selectors.player);
     if (!miniPlayer.hasAttribute('playing')) {
       miniPlayer.toggleAttribute('playing');
     }
@@ -218,9 +252,9 @@ export default class AudioPlayer {
    * @returns {void}
    */
   _clearPlaying() {
-    document.querySelector('.player').removeAttribute('playing');
-    const all = document.querySelectorAll('#stations>li');
-    all.forEach(el => el.removeAttribute('playing'));
+    document.querySelector(this._selectors.player).removeAttribute('playing');
+    const allStations = document.querySelectorAll(this._selectors.stations);
+    allStations.forEach(el => el.removeAttribute('playing'));
     this.currentPlayingElement = null;
   }
 
@@ -232,7 +266,7 @@ export default class AudioPlayer {
    * @returns {void}
    */
   _handleOffline() {
-    const playerElement = document.querySelector('.player');
+    const playerElement = document.querySelector(this._selectors.player);
     if (playerElement.hasAttribute('playing')) {
       new Toast('Disconnected: attempting reconnect', 1.5);
     }
@@ -246,7 +280,7 @@ export default class AudioPlayer {
    * @returns {void}
    */
   _handleOnline() {
-    const playerElement = document.querySelector('.player');
+    const playerElement = document.querySelector(this._selectors.player);
     if (playerElement.hasAttribute('playing')) {
       new Toast('Reconnected: attempting to restart play', 1.5);
       this.player.play();
@@ -254,23 +288,60 @@ export default class AudioPlayer {
   }
 
   /**
+   * keypress callback function
+   * 
+   * @param {Event} ev 
+   */
+  _onKeyPress(ev) {
+    ev.preventDefault();
+    const button = ev.code;
+    switch (button) {
+      case 'Space': 
+        if (this.player.src) this._bouncedToggle();
+        break;
+    }
+  }
+
+  /**
+   * remove event listeners
+   */
+  destroy() {
+    window.removeEventListener('offline', this._handleOffline.bind(this));
+  
+    window.removeEventListener('online', this._handleOnline.bind(this));
+
+    window.removeEventListener('keypress', this._onKeyPress);
+
+    document.querySelector(this._selectors.name).removeEventListener('click', this._scrollToStation);
+
+    document.querySelector(this._selectors.smallButton).removEventListener('click', this._togglePlay);
+    
+    document.querySelector('#vol>input').removeEventListener('input', this._setVolume);
+  }
+
+  /**
    * loads player into document
    */
   async init() {
     document.querySelector('body').append(this.player);
-    document.querySelector('.player>.small-button').addEventListener('click', this._togglePlay.bind(this));
-
+    
     const notMobile = await this._canChangeVol();
-
+    
     if (notMobile) {
       this._setVolumeSlider();
     } else {
-      const volumeElement = document.querySelector('#vol');
+      const volumeElement = document.querySelector(this._selectors.volume);
       volumeElement.style.display = 'none';
     }
-  
+    
     window.addEventListener('offline', this._handleOffline.bind(this));
-  
+    
     window.addEventListener('online', this._handleOnline.bind(this));
+    
+    window.addEventListener('keypress', this._onKeyPress);
+    
+    document.querySelector(this._selectors.name).addEventListener('click', this._scrollToStation);
+
+    document.querySelector(this._selectors.smallButton).addEventListener('click', this._togglePlay);
   }
 }
