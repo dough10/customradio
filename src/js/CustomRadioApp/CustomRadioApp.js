9515eeb3c584f96b00c7c03c6056c3622ded5d0b
dough10 <doughten.jimi@gmail.com>
2025-04-09 21:10:25 -0500

    rm call ro moved function

--- src/js/CustomRadioApp/CustomRadioApp.js
+++ src/js/CustomRadioApp/CustomRadioApp.js
@@ -57,8 +57,6 @@ export default class CustomRadioApp {
       onReset: this._resetFilter.bind(this)
     });
     
-    destroyDialogInteractions();
-    
     if (this._lzldr) {
       this._lzldr.destroy();
     }
