83a32ae236e17c8607c986d16c2d3fe348b26a72
dough10 <doughten.jimi@gmail.com>
2025-04-12 15:34:09 -0500

    grab all language files in folder

--- util/i18n.js
+++ util/i18n.js
@@ -1,37 +1,36 @@
 // i18n.js
+const fs = require('fs');
+const path = require('path');
 
 const Logger = require('./logger.js');
 
 const logLevel = process.env.LOG_LEVEL || 'info';
 const log = new Logger(logLevel);
 
-const en = require('../locales/en');
+const locales = {};
+const localesDir = path.join(__dirname, '..', 'locales');
 
-const languages = { en };
+fs.readdirSync(localesDir).forEach(file => {
+  const lang = path.basename(file, '.js');
+  locales[lang] = require(path.join(localesDir, file));
+});
 
 let currentLang = 'en';
 
-function setLanguage(langCode) {
-  if (languages[langCode]) {
-    currentLang = langCode;
+function setLanguage(lang) {
+  if (locales[lang]) {
+    currentLang = lang;
   } else {
-    log.warning(`Language "${langCode}" not found. Falling back to English.`);
+    log.warning(`Language "${lang}" not found. Falling back to 'en'.`);
     currentLang = 'en';
   }
+  return currentLang;
 }
 
 function t(key, ...args) {
-  const langPack = languages[currentLang];
-  const translation = langPack[key];
-
-  if (typeof translation === 'function') {
-    return translation(...args);
-  }
-
-  return translation || key;
+  const value = locales[currentLang][key];
+  return typeof value === 'function' ? value(...args) : value || key;
 }
 
-module.exports = {
-  setLanguage,
-  t,
-};
+module.exports = { setLanguage, t };
+
