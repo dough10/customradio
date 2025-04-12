ac2f802d5f7154ba11f1c28d497fdb8f44cd02d7
dough10 <doughten.jimi@gmail.com>
2025-04-12 15:33:19 -0500

    seperate by sections

--- locales/en.js
+++ locales/en.js
@@ -1,15 +1,24 @@
 module.exports = {
   clickDismiss: 'Click to dismiss',
+  
   title: 'radio.txt creator',
   intro: 'A website for creating a custom radio.txt for use with ',
   hibyLink: 'Hiby digital audio players',
   siteUse: 'To use the site',
   step1: 'Filter stations by name or genre (e.g., somafm, hip hop, jazz), then add all the stations you would like to include in your radio.txt file.',
   step2: 'When you are happy with your list, press "Download" and save the text file to the root directory of the storage on your Hiby player.',
+  closeButtonText: 'close',
+
   filterLabel: 'Filter by name or genre',
   downloadButtonText: 'download',
-  closeButtonText: 'close',
   volume: 'Volume',
+
   thanks: 'Thank you for using the site. Please report any security concerns to the contact information below.',
   securityContact: 'Security contact',
+
+  addStation: 'add station',
+  addCase1: 'URL should be an Icecast server with a content type of "audio/mpeg" or "audio/mp3"',
+  addCase2: 'The API will get the other information from the stream headers.',
+  stationURL: 'Station URL',
+  addButtonText: 'add'
 };
