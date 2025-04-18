5bd1fc3f53985013cacf2f4832f8a3b36a994e2a
dough10 <doughten.jimi@gmail.com>
2025-04-18 06:15:22 -0500

    js docs

--- util/i18n.js
+++ util/i18n.js
@@ -7,16 +7,28 @@ const Logger = require('./logger.js');
 const logLevel = process.env.LOG_LEVEL || 'info';
 const log = new Logger(logLevel);
 
+/** @type {Object} an object of language file names & file paths locales[en] = 'path to app folder/locales/en.js' */
 const locales = {};
-const localesDir = path.join(__dirname, '..', 'locales');
 
+/**
+ * load translation files
+ */
+const localesDir = path.join(__dirname, '..', 'locales');
 fs.readdirSync(localesDir).forEach(file => {
   const lang = path.basename(file, '.js');
   locales[lang] = require(path.join(localesDir, file));
 });
 
+/** @type {String} current language */
 let currentLang = 'en';
 
+/**
+ * attempt to set language to users prefrence
+ * 
+ * @param {String} lang 
+ * 
+ * @returns {String}
+ */
 function setLanguage(lang) {
   if (locales[lang]) {
     currentLang = lang;
@@ -27,6 +39,14 @@ function setLanguage(lang) {
   return currentLang;
 }
 
+/**
+ * grabs the string or function assigned to input key
+ * 
+ * @param {String} key 
+ * @param  {...any} args 
+ * 
+ * @returns {String}
+ */
 function t(key, ...args) {
   const value = locales[currentLang][key];
   return typeof value === 'function' ? value(...args) : value || key;
