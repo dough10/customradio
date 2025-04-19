6384403f846d797a1704ac2dcc14cce6d4a9f6f4
dough10 <doughten.jimi@gmail.com>
2025-04-19 17:57:52 -0500

    cover more

--- src/js/CustomRadioApp/utils/normalizeInput.js
+++ src/js/CustomRadioApp/utils/normalizeInput.js
@@ -7,8 +7,9 @@
 export default function normalizeInput(str) {
   return str
     .toLowerCase()
-    .replace(/&|\band\b|(?:\bn\b|'n|n')(?=\s|$)/g, ' and ')
+    .replace(/&|\band\b|(?:\bn\b|'n'|n'|'n)(?=\s|$|\W)/g, ' and ')
     .normalize("NFD").replace(/[\u0300-\u036f]/g, '')
+    .replace(/[-\/\\]/g, ' ')
     .replace(/[^a-z0-9\s]/gi, '')
     .split(/\s+/)
     .filter(Boolean)
