15e5f341f4ff9567f58be72b7bfa27f9ddf7f032
dough10 <doughten.jimi@gmail.com>
2025-05-03 10:26:17 -0500

    feat(postOptions): add utility function for standardized POST request options

---
+++ src/js/CustomRadioApp/utils/post_options.js
@@ -0,0 +1,17 @@
+export default async (body = null) => {
+  const options = {
+    method: 'POST',
+    credentials: 'same-origin',
+    headers: {
+      'X-Requested-With': 'XMLHttpRequest',
+      'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]')?.content
+    }
+  };
+
+  if (body) {
+    options.headers['Content-Type'] = 'application/x-www-form-urlencoded';
+    options.body = body;
+  }
+
+  return options;
+};
