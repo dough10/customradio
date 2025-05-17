749d7d48ca0ab37858fba798084540912a301a09
dough10 <doughten.jimi@gmail.com>
2025-05-17 15:41:29 -0500

    admin email

--- routes/endpoints/securitytxt.js
+++ routes/endpoints/securitytxt.js
@@ -5,7 +5,7 @@ const log = new Logger(logLevel);
 
 module.exports = (req, res) => {
   const strings = [
-    'Contact: mailto:doughten.jimi@gmail.com',
+    'Contact: mailto:admin@dough10.me',
     'Expires: 2026-01-01T06:00:00.000Z'
   ];
   log.info(`${req.ip} -> /.well-known/security.txt ${Date.now() - req.startTime}ms`);
