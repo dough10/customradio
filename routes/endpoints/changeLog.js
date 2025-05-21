739e8416851ddf0eda6ae1eeea1917fdd3811912
dough10 <doughten.jimi@gmail.com>
2025-05-21 09:52:49 -0500

    start a change log could be a txt endpoint

---
+++ routes/endpoints/changeLog.js
@@ -0,0 +1,9 @@
+const log = {
+  "1.12.1": [
+    'Added an Alert system to notify users of changes comming to the site',
+    'Anonymous station play reporting, shifts popular stations to top of search results',
+    'Japaneese translations',
+    'Korean Translations',
+    'Swedish translations',
+  ] 
+}
