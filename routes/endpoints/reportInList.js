4573eac13697f2fe04c786f8f277a63eee06246b
dough10 <doughten.jimi@gmail.com>
2025-06-02 18:39:19 -0500

    blacklist

--- routes/endpoints/reportInList.js
+++ routes/endpoints/reportInList.js
@@ -5,10 +5,19 @@ const { t } = require('../../util/i18n.js');
 const logLevel = process.env.LOG_LEVEL || 'info';
 const log = new Logger(logLevel);
 
+const blacklist = process.env.BLACKLIST?.split(',') || [];
+
 module.exports = async (req, res) => {
+  const ip = req.ip;
   const id = req.params.id;
   const state = Number(req.params.state);
   const sql = new Stations('data/customradio.db');
+
+  if (state && blacklist.includes(ip)) {
+    res.json({ message: 'ip in blacklist' });
+    return;
+  }
+
   try {
     if (state) {
       await sql.addToList(id);
