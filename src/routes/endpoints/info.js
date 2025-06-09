7d7c7e7381b4ba94ab293ce3f0af6f4a70dc5b3c
dough10 <doughten.jimi@gmail.com>
2025-06-09 06:29:54 -0500

    rm un used

--- src/routes/endpoints/info.js
+++ src/routes/endpoints/info.js
@@ -1,5 +1,3 @@
-const fs = require('fs');
-const path = require('path');
 const Logger = require('../../util/logger.js');
 const changelog = require('../../../changelog.json');
 const pack = require('../../../package.json');
