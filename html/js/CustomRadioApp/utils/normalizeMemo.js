e7db5c28d34c2bc54cd69155d439155ee7615200
dough10 <doughten.jimi@gmail.com>
2025-06-09 09:12:11 -0500

    slipt string normalize then reaccemble

--- html/js/CustomRadioApp/utils/normalizeMemo.js
+++ html/js/CustomRadioApp/utils/normalizeMemo.js
@@ -6,7 +6,7 @@ const cache = new Map();
 export default function normalizeMemo(str) {
   if (!str) return '';
   if (cache.has(str)) return cache.get(str);
-  const normalized = normalizeInput(str);
+  const normalized = str.split(',').map(part => normalizeInput(part)).join(',');
   cache.set(str, normalized);
   return normalized;
 }
