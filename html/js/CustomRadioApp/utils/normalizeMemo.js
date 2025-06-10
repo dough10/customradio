599ccb9b08551129fb2ed737f86e13fbba1ece72
dough10 <doughten.jimi@gmail.com>
2025-06-10 11:47:36 -0500

    indenting

--- html/js/CustomRadioApp/utils/normalizeMemo.js
+++ html/js/CustomRadioApp/utils/normalizeMemo.js
@@ -6,7 +6,9 @@ const cache = new Map();
 export default function normalizeMemo(str) {
   if (!str) return '';
   if (cache.has(str)) return cache.get(str);
-  const normalized = str.split(',').map(part => normalizeInput(part)).join(',');
+  const normalized = str.split(',').map(part => 
+    normalizeInput(part)
+  ).join(',');
   cache.set(str, normalized);
   return normalized;
 }
