6a2cbff0afd3cea07b10b949766ac6646095af77
dough10 <doughten.jimi@gmail.com>
2025-07-11 20:26:52 -0500

    this seems to be the root of the dm update issue

--- config/runtime.config.js
+++ config/runtime.config.js
@@ -20,19 +20,12 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
+        exclude: /node_modules/
       },
     ],
   },
   resolve: {
     extensions: ['.js'],
   },
-  // Exclude all node_modules dependencies
-  externals: nodeModules,
+  externals: nodeModules
 };
