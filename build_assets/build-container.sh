528252c94b86c97667f564a03d6c6b79ce5d25f2
dough10 <doughten.jimi@gmail.com>
2025-07-30 06:24:20 -0500

    rm leading space

--- build_assets/build-container.sh
+++ build_assets/build-container.sh
@@ -15,5 +15,5 @@ if [ -z "$1" ]; then
   echo "Usage: $0 <image-name>"
   exit 1
 fi
- docker build -t "$1:$VERSION" -t "$1:latest" .
-#  docker push "$1:$VERSION" && docker push "$1:latest"
+docker build -t "$1:$VERSION" -t "$1:latest" .
+# docker push "$1:$VERSION" && docker push "$1:latest"
