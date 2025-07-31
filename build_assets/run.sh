4e5887da93a085a4757979e8a54d5fda38e50802
dough10 <doughten.jimi@gmail.com>
2025-07-31 06:23:47 -0500

    refactor: update secret generation and environment variable setup in run.sh

--- build_assets/run.sh
+++ build_assets/run.sh
@@ -2,13 +2,23 @@
 
 source .env
 
-secret=$(openssl rand -base64 16 | tr -d '/+' | cut -c1-16)
+generate_secret() {
+  openssl rand -base64 32 | tr -d '/+' | cut -c1-32
+}
+
+jwt_secret=$(generate_secret)
+session_secret=$(generate_secret)
+cookie_secret=$(generate_secret)
 
 docker run \
   --name=testradio \
   --env=REDIS_URL="$REDIS_URL" \
   --env=REDIS_PASSWORD="$REDIS_PASSWORD" \
-  --env=SESSION_SECRET="$secret" \
+  --env=WORKOS_API_KEY="$WORKOS_API_KEY" \
+  --env=WORKOS_CLIENT_ID="$WORKOS_CLIENT_ID" \
+  --env=COOKIE_SECRET="$cookie_secret" \
+  --env=JWT_SECRET="$jwt_secret" \
+  --env=SESSION_SECRET="$session_secret" \
   --env=LOG_LEVEL="$LOG_LEVEL" \
   --env=AUTH_SERVER_URL="$AUTH_SERVER_URL" \
   --env "$BLACKLIST" \
