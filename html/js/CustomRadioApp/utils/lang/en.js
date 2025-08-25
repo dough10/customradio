18a96107fc34c11e8b6a734ebcac549062de84b7
dough10 <doughten.jimi@gmail.com>
2025-08-25 07:53:40 -0500

    rm whitespace

--- html/js/CustomRadioApp/utils/lang/en.js
+++ html/js/CustomRadioApp/utils/lang/en.js
@@ -2,7 +2,6 @@ export const en = {
   stations: (count) => `${count} stations`,
   genreError: 'Invalid input. Please enter valid genre.',
   stationsError: error => `Error fetching stations: ${error}`,
-
   playing: name => `Playing: ${name}`,
   homepage: 'homepage',
   homepageTitle: homepage => `navigate to ${homepage}`, 
@@ -14,14 +13,11 @@ export const en = {
   invalidStation: `Invalid station data. Unable to play stream.`,
   offline: 'Disconnected: attempting reconnect',
   online: 'Reconnected: attempting to restart play',
-
   playTitle: 'Play stream',
   addTitle: 'Add to file', 
   removeTitle: 'Remove from file',
-
   appUpdated: 'App updated',
   pressToRefresh: 'Press to refresh',
-
   dismiss: 'Click to dismiss',
   news: `
     <h2>We've Updated – User List Storage!</h2>
