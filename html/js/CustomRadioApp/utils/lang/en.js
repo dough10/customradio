8f46fb6b2c6df62fd90e34cfe043172ad50f2fa9
dough10 <doughten.jimi@gmail.com>
2025-08-29 17:28:27 -0500

    refactor language strings for consistency and clarity

--- html/js/CustomRadioApp/utils/lang/en.js
+++ html/js/CustomRadioApp/utils/lang/en.js
@@ -1,5 +1,5 @@
 export const en = {
-  stations: (count) => `${count} stations`,
+  stations: count => `${count} stations`,
   genreError: 'Invalid input. Please enter valid genre.',
   stationsError: error => `Error fetching stations: ${error}`,
   playing: name => `Playing: ${name}`,
@@ -10,7 +10,7 @@ export const en = {
   playingError: error => `Error playing media: ${error}`,
   noHome: 'No homepage',
   errorHome: error => `Error opening homepage: ${error}`,
-  invalidStation: `Invalid station data. Unable to play stream.`,
+  invalidStation: 'Unable to play stream. Invalid station data',
   offline: 'Disconnected: attempting reconnect',
   online: 'Reconnected: attempting to restart play',
   playTitle: 'Play stream',
