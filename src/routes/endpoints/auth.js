8b60f0961dde9a0b0d5a249e66399d3c048deea4
dough10 <doughten.jimi@gmail.com>
2025-09-08 20:06:01 -0500

    fix redirect

--- src/routes/endpoints/auth.js
+++ src/routes/endpoints/auth.js
@@ -2,7 +2,7 @@ const { WorkOS } = require('@workos-inc/node');
 
 const workos = new WorkOS(process.env.WORKOS_API_KEY);
 const clientId = process.env.WORKOS_CLIENT_ID;
-const redirectUri = 'https://testradio.dough10.me/auth/callback';
+const redirectUri = 'https://radiotxt.site/auth/callback';
 
 
 module.exports = (req, res) => {
