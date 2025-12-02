6c0aa9d2b3ea5dc74b472e6f7ae64b8a831430e9
dough10 <doughten.jimi@gmail.com>
2025-12-02 17:44:23 -0600

    Remove redundant enabling of submit button after form submission error handling

--- html/js/CustomRadioApp/UIManager/dialogs/dialog.js
+++ html/js/CustomRadioApp/UIManager/dialogs/dialog.js
@@ -101,8 +101,6 @@ async function submitStation(ev) {
     if (typeof _paq !== 'undefined') {
       _paq.push(['trackEvent', 'Error', e.message || 'Could not get Message']);
     }
-  } finally {
-    submit.removeAttribute('disabled');
   }
 }
 
