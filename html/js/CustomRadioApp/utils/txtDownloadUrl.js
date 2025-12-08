3125ce4c636caab5b121703295c762cff1cb1272
dough10 <doughten.jimi@gmail.com>
2025-12-08 06:10:38 -0600

    return null

--- html/js/CustomRadioApp/utils/txtDownloadUrl.js
+++ html/js/CustomRadioApp/utils/txtDownloadUrl.js
@@ -5,8 +5,7 @@
  */
 export default function txtDownloadUrl() {
   if (!window.user || !window.user.id) {
-    console.warn('User ID not found in global window object');
-    return '';
+    return;
   }
   return new URL(
     `/txt/${window.user.id.replace('user_', '')}`, 
