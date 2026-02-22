078312cec9b0805a13e41d13e42c6869606c085f
dough10 <doughten.jimi@gmail.com>
2026-02-22 03:23:43 -0600

    Add response codes for better error handling in add.js

--- src/routes/endpoints/add.js
+++ src/routes/endpoints/add.js
@@ -8,6 +8,29 @@ const { t } = require('../../util/i18n.js');
 const logLevel = process.env.LOG_LEVEL || 'info';
 const log = new Logger(logLevel);
 
+const RESPONSE_CODES = Object.freeze({
+  200: 'OK: The request was successful.',
+  201: 'Created: The station was successfully added.',
+  202: 'Accepted: The request has been accepted for processing, but the processing is not complete.',
+  203: 'Non-Authoritative Information: The request was successful but the returned information may be from a third-party source.',
+  204: 'No Content: The request was successful but there is no content to send in the response.',
+  205: 'Reset Content: The request was successful but the client should reset the view.',
+  206: 'Partial Content: The server is delivering only part of the resource due to a range header sent by the client.',
+  400: 'Bad Request: The server can’t process your request.',
+  401: 'Unauthorized: The server rejected your request due to missing or invalid authentication. ',
+  402: 'Payment Required: The server rejected your request due to missing or invalid authentication.',
+  403: 'Forbidden: The server understood your request but denied access.',
+  404: 'Not Found: The requested resource could not be found.',
+  405: 'Method Not Allowed: The request method is not supported for the requested resource.',
+  406: 'Not Acceptable: The requested resource is not available in a format acceptable to the client.',
+  408: 'Request Timeout: The server timed out waiting for the request.',
+  409: 'Conflict: The request could not be completed due to a conflict with the current state of the resource.',
+  410: 'Gone: The requested resource is no longer available and will not be available again.',
+  411: 'Length Required: The server refuses to accept the request without a defined Content-Length.',
+  500: 'Internal Server Error: The server encountered an unexpected condition that prevented it from fulfilling the request.',
+});
+
+
 /**
  * Handles the request to add a new station to the database.
  * 
@@ -61,7 +84,7 @@ module.exports = async (req, res) => {
     const status = await isLiveStream(url);
     if (!status.ok) {
       log.warning(`Connection test failed: ${status.error}`);
-      return res.status(400).json({ message: t('conTestFailed', status.error)});
+      return res.status(400).json({ message: t('conTestFailed', RESPONSE_CODES[status.error]) });
     }
 
     const data = {
