272fb4bcb1f2736634843eba34167245cc5f399d
dough10 <doughten.jimi@gmail.com>
2026-02-24 07:15:54 -0600

    Add URL blocking logic to isValidURL function for improved validation

--- src/util/isValidURL.js
+++ src/util/isValidURL.js
@@ -1,8 +1,21 @@
+const blockedPatterns = [
+  /^(https?:\/\/)?(localhost|127\.\d+\.\d+\.\d+)/,
+  /^(https?:\/\/)?192\.168\.\d+\.\d+/,
+  /^(https?:\/\/)?10\.\d+\.\d+\.\d+/,
+  /^(https?:\/\/)?172\.(1[6-9]|2[0-9]|3[01])\.\d+\.\d+/,
+  /^(https?:\/\/)?169\.254\.\d+\.\d+/,
+];
+
 module.exports = (url) => {
+  const isBlocked = blockedPatterns.some(pattern => pattern.test(url));
+  if (isBlocked) {
+    return false;
+  }
+  
   try {
     new URL(url);
     return true;
   } catch (_) {
     return false;
   }
-}
+};
