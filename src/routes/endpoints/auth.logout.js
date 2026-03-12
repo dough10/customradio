8bca739fbfc74d9bb821a66c2d2b45d672470827
dough10 <doughten.jimi@gmail.com>
2026-03-12 04:24:23 -0500

    reset req.user check for sessions before attempt to logout

--- src/routes/endpoints/auth.logout.js
+++ src/routes/endpoints/auth.logout.js
@@ -1,15 +1,25 @@
 const { WorkOS } = require('@workos-inc/node');
+const asyncHandler = require('../../util/asyncHandler.js');
 
 const clientId = process.env.WORKOS_CLIENT_ID;
-const asyncHandler = require('../../util/asyncHandler.js');
+
+const COOKIE_NAME = 'wos-session';
 
 const workos = new WorkOS(process.env.WORKOS_API_KEY, {
   clientId,
 });
 
 module.exports = asyncHandler(async (req, res) => {
+  if (req.user) req.user = null;
+
+  const sessionCookie = req.cookies[COOKIE_NAME];
+
+  if (!sessionCookie) {
+    return res.redirect('/');
+  }
+
   const session = workos.userManagement.loadSealedSession({
-    sessionData: req.cookies['wos-session'],
+    sessionData: sessionCookie,
     cookiePassword: process.env.COOKIE_SECRET,
   });
 
