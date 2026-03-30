214b43738ab85e0cd6ab73ab4ef58f1493537595
dough10 <doughten.jimi@gmail.com>
2026-03-30 14:00:33 -0500

    adsfaf

--- src/util/isAdmin.js
+++ src/util/isAdmin.js
@@ -1,5 +1,5 @@
 module.exports = ({user}) => {
-  console.log(user, user?.metadata);
+  // console.log(user, user?.metadata);
   const adminID = process.env.ADMIN_ID;
   if (!adminID || !user) return false;
   return adminID === user.id;
