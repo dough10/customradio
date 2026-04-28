aa5e49d5730f1201bb44b40c0f6de31108a6b6d3
dough10 <doughten.jimi@gmail.com>
2026-04-27 22:11:41 -0500

    record for non users

--- src/routes/endpoints/saveStation.js
+++ src/routes/endpoints/saveStation.js
@@ -4,12 +4,7 @@ const asyncHandler = require('../../util/asyncHandler.js');
 const blacklist = process.env.BLACKLIST?.split(',') || [];
 
 module.exports = asyncHandler(async (req, res) => {
-  const userID = req.user ? req.user.id : null;
-  if (!userID) {
-    res.status(204).send();
-    return;
-  }
-  
+  const userID = req.user ? req.user.id : null;  
   const stationID = req.params.id;
   
   if (!stationID || isNaN(stationID)) {
@@ -18,13 +13,14 @@ module.exports = asyncHandler(async (req, res) => {
   }
   
   const state = req.query.state === '1' ? 1 : 0;
+  const blackListed = blacklist.includes(req.ip);
   
   if (state) {
-    if (!blacklist.includes(req.ip)) await stations.addToList(stationID);
-    await userData.addStation(userID, stationID);
+    if (!blackListed) await stations.addToList(stationID);
+    if (userID) await userData.addStation(userID, stationID);
   } else {
-    await stations.removeFromList(stationID);
-    await userData.removeStation(userID, stationID);
+    if (!blackListed) await stations.removeFromList(stationID);
+    if (userID) await userData.removeStation(userID, stationID);
   }
   res.status(204).send();
 });
