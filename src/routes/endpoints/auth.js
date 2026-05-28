3ecd4298f06f523b8ce66529dcb76425d66d63d5
dough10 <doughten.jimi@gmail.com>
2026-05-27 20:52:34 -0500

    userManagment

--- src/routes/endpoints/auth.js
+++ src/routes/endpoints/auth.js
@@ -1,9 +1,10 @@
 const {workos} = require('../../services.js');
 
 module.exports = (req, res) => {
-  res.redirect(workos.sso.getAuthorizationUrl({
+  res.redirect(workos.userManagement.getAuthorizationUrl({
     provider: 'authkit',
     redirectUri: process.env.WORKOS_REDIRECT_URL,
-    clientId: process.env.WORKOS_CLIENT_ID
+    clientId: process.env.WORKOS_CLIENT_ID,
+    screenHint: 'sign-in'
   }));
 };
