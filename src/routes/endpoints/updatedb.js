19d96f439e29b2e252d6fbf579c34d670a94a22c
dough10 <doughten.jimi@gmail.com>
2026-06-15 02:52:44 -0500

    text not object

--- src/routes/endpoints/updatedb.js
+++ src/routes/endpoints/updatedb.js
@@ -3,7 +3,7 @@ const asyncHandler = require('../../util/asyncHandler.js');
 const isAdmin = require('./../../util/isAdmin.js');
 
 module.exports = asyncHandler(async (req, res) => {
-  if (!isAdmin(req)) return res.status(403).json({message:'Forbidden'});
+  if (!isAdmin(req)) return res.status(403).send('Forbidden');
   testStreams();
   res.json({
     message: 'update began.'
