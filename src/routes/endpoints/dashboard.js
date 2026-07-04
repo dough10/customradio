62641da86d5509e5fd42d788cb9c3b2ca68b69fd
dough10 <doughten.jimi@gmail.com>
2026-07-03 19:15:45 -0500

    redirect back here

--- src/routes/endpoints/dashboard.js
+++ src/routes/endpoints/dashboard.js
@@ -3,13 +3,19 @@ const pug = require('pug');
 const asyncHandler = require('../../util/asyncHandler.js');
 const isAdmin = require('./../../util/isAdmin.js');
 
+const { mongo } = require('../../services.js');
+
 module.exports = asyncHandler(async (req, res) => {
+  const user = req.user;
+  if (!user) {
+    res.redirect('/auth?ref=/dashboard');
+    return;
+  }
   const admin = isAdmin(req);
   if (!admin) {
     res.redirect('/');
     return;
   };
-  const user = req.user;
   res.send(pug.renderFile('./templates/dashboard.pug', {
     user: user ? {
       id: user.id,
@@ -22,5 +28,6 @@ module.exports = asyncHandler(async (req, res) => {
     lang: req.loadedLang,
     csrf: req.session.csrfToken,
     nonce: res.locals.nonce,
+    requests: await mongo.getRequestCounts(24)
   }));
 });
