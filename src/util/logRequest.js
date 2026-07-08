defbe4885d497ca0e509ef402c2b57f4277c5054
dough10 <doughten.jimi@gmail.com>
2026-07-08 17:35:27 -0500

    clarity

--- src/util/logRequest.js
+++ src/util/logRequest.js
@@ -12,10 +12,18 @@ const DEFAULT = {
 
 module.exports = function logRequest(req, res=DEFAULT, ms) {
   const uaParser = new UAParser(req.headers['user-agent']);
+
+  // create new request object with masked ip address 
   const newReq = {...req, ip: maskIP(req.ip)};
+  
+  // create variables
   const { ip, method, originalUrl } = newReq;
   const { path, query } = parsePath(originalUrl);
+  
+  // log in mognodb
   mongo.logRequest(ip, method, path, query, res.statusCode, uaParser.getResult(), ms)
     .catch(err => logger.error(`Failed to log request: ${err}`));
+  
+  // application logger
   logger.info(requestString(newReq, res, ms));
 }
